6c3d0c72a3a1f628c8c875fef2e00a0bc33cda4c
Kwasniok <JensKwasniok@web.de>
2015-12-23 21:17:59 +0100

    Initial Commit

---
+++ PenAndConsole/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PenAndConsole
+//
+//  Created by Jens Kwasniok on 23.12.15.
+//  Copyright (c) 2015 Jens Kwasniok. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
