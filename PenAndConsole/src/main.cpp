b9c59f335f5577f4030afd273efb4be3a3d98f29
Kwasniok <JensKwasniok@web.de>
2016-01-09 22:00:04 +0100

    moved tests into separate file

--- PenAndConsole/src/main.cpp
+++ PenAndConsole/src/main.cpp
@@ -13,6 +13,9 @@
 #include "File_Writer.hpp"
 #include "File_Reader.hpp"
 
+#include "tests.hpp"
+
+
 using namespace std;
 
 bool begins_with(const std::string& str, const std::string& beg);
@@ -25,38 +28,10 @@ void print_inventory(Console& c, Context& cxt);
 
 int main(int argc, const char * argv[]) {
 
-	// Various Tests //
-	// basic setup
+	if (do_tests()) return EXIT_SUCCESS;
+
 	Console c {cin, cout};
 	Context cxt;
-	 
-	// Various Tests //
-	/*
-	// simple world
-	cxt.context_vars["door"] = "closed";
-	cxt.inventory.items.push_back({"stick"});
-	cxt.actions.push_back({"hello", {}, {}, {"no respond"}});
-	cxt.actions.push_back({"open door",  {{"door", "closed"}}, {}, {"crunching noise", {{"door", "opend" }}, {}}});
-	cxt.actions.push_back({"close door", {{"door", "opend" }}, {}, {"crunching noise", {{"door", "closed"}}, {}}});
-	cxt.actions.push_back({"grab stone", {}, {}, {"grabbed stone", {}, {{"stone"}}, {}}});
-	cxt.actions.push_back({"throw stone", {}, {{"stone"}}, {"off it goes", {}, {}, {{"stone"}}}});
-	*/
-
-	// IO tests
-	/*
-	File_Writer fw1 {"/Users/Jens/Datein/Xcode/PenAndConsole/PenAndConsole/tmp/save.txt"};
-	fw1 << cxt;
-	Inventory i3;
-	Context cxt3;
-	//
-	File_Reader fr4 {"/Users/Jens/Datein/Xcode/PenAndConsole/PenAndConsole/tmp/save.txt"};
-	vector<Block> bs;
-	fr4 >> bs;
-	Context cxt4;
-	bs[0] >> cxt4;
-	File_Writer fw4 {"/Users/Jens/Datein/Xcode/PenAndConsole/PenAndConsole/tmp/save4.txt"};
-	fw4 << cxt4;
-	 */
 
 	bool show_possible_actions_permanently = false;
 	const std::string path_save = "/Users/Jens/Datein/Xcode/PenAndConsole/PenAndConsole/tmp/";
